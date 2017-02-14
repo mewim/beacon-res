7aa6931949c3bd8914d30e2db31cf0dfa56527f5
Liu, Chang <cliu81@illinois.edu>
2017-02-14 14:06:24 -0600

    Insert version to MongoDB.

--- api/beacons.js
+++ api/beacons.js
@@ -15,13 +15,30 @@ router.get('/', function (req, res) {
 router.get('/sync', function (req, res) {
     request('http://minrva-dev.library.illinois.edu:8080/estimote/rest/v1.0/version', function (error, response, body) {
         if (!error && response.statusCode == 200) {
-            console.log(body);
+            var parsed_version = JSON.parse(body);
+            DB.conn(function (db) {
+                var beacons_version = db.collection('beacons_version');
+                beacons_version.remove({}, function (error, number_removed) {
+                    beacons_version.insert(parsed_version, function (error, result) {
+                        console.log('Beacon version synced.');
+                        var beacons_version = db.collection('beacons');
+                        request('http://minrva-dev.library.illinois.edu:8080/estimote/rest/v1.0/beacons', function (error, response, body) {
+                            var beacons = db.collection('beacons');
+                            beacons_version.remove({}, function (error, number_removed) {
+                                if (!error && response.statusCode == 200) {
+                                    var parsed_beacons = JSON.parse(body);
+                                    console.log(body);
+                                    res.status(200).send({
+                                        success: true,
+                                        message: "Sync completed."
+                                    });
+                                }
+                            });
+                        });
+                    });
+                });
+            });
         }
-
-    });
-    res.status(200).send({
-        success: true,
-        message: "Sync completed."
     });
 });
 
