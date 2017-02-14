9e76947695e7f1324e976d07c3e8cb8c2cc4e303
Liu, Chang <cliu81@illinois.edu>
2017-02-14 11:43:58 -0600

    Fix another API bug.

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@
 var express = require('express'),
     router = express.Router();
 
-router.post('/', JsonParser, function (req, res) {
+router.post('/', function (req, res) {
  res.status(200).send({
                 success: true,
                 message: "Hello world."
