726e4b801719e400f64cac912f2b756ea76dc4ba
Liu, Chang <cliu81@illinois.edu>
2017-02-14 14:20:47 -0600

    Sync?

--- api/beacons.js
+++ api/beacons.js
@@ -19,24 +19,72 @@ router.get('/sync', function (req, res) {
             DB.conn(function (db) {
                 var beacons_version = db.collection('beacons_version');
                 beacons_version.remove({}, function (error, number_removed) {
-                    beacons_version.insert(parsed_version, function (error, result) {
-                        console.log('Beacon version synced.');
-                        var beacons_version = db.collection('beacons');
-                        request('http://minrva-dev.library.illinois.edu:8080/estimote/rest/v1.0/beacons', function (error, response, body) {
-                            var beacons = db.collection('beacons');
-                            beacons_version.remove({}, function (error, number_removed) {
-                                if (!error && response.statusCode == 200) {
-                                    var parsed_beacons = JSON.parse(body);
-                                    console.log(body);
-                                    res.status(200).send({
-                                        success: true,
-                                        message: "Sync completed."
-                                    });
-                                }
-                            });
+                    if (error) {
+                        res.status(500).send({
+                            success: false,
+                            message: "Cannot remove old beacon version."
                         });
-                    });
+                    }
+                    else {
+                        beacons_version.insert(parsed_version, function (error, result) {
+                            if (error) {
+                                res.status(500).send({
+                                    success: false,
+                                    message: "Cannot insert new beacon version."
+                                });
+                            }
+                            else {
+                                console.log('Beacon version synced.');
+                                var beacons_version = db.collection('beacons');
+                                request('http://minrva-dev.library.illinois.edu:8080/estimote/rest/v1.0/beacons', function (error, response, body) {
+                                    var beacons = db.collection('beacons');
+                                    beacons_version.remove({}, function (error, number_removed) {
+                                        if (!error && response.statusCode == 200) {
+                                            var parsed_beacons = JSON.parse(body);
+                                            var beacons = db.collection('beacons');
+                                            beacons.remove({}, function (error, number_removed) {
+                                                if (error) {
+                                                    res.status(500).send({
+                                                        success: false,
+                                                        message: "Cannot remove old beacons."
+                                                    });
+                                                }
+                                                else {
+                                                    beacons.insertMany(parsed_beacons, function (error, result) {
+                                                        if (error) {
+                                                            res.status(500).send({
+                                                                success: false,
+                                                                message: "Cannot insert new beacons."
+                                                            });
+                                                        }
+                                                        else {
+                                                            res.status(200).send({
+                                                                success: true,
+                                                                message: "Sync completed."
+                                                            });
+                                                        }
+                                                    });
+                                                }
+                                            });
+                                        }
+                                        else {
+                                            res.status(500).send({
+                                                success: false,
+                                                message: "Cannot download beacons from remote server."
+                                            });
+                                        }
+                                    });
+                                });
+                            }
+                        });
+                    }
                 });
+            });
+        }
+        else {
+            res.status(500).send({
+                success: false,
+                message: "Cannot download beacon version from remote server."
             });
         }
     });
