7bf82809a8a15e51bf3a6c09d577249312f081d0
Liu, Chang <cliu81@illinois.edu>
2017-02-14 13:34:51 -0600

    Fix URL of db.

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 var MongoClient = require('mongodb').MongoClient;
 
-var DB_PATH = "admin:12345678@ds153239.mlab.com:53239/heroku_j86p4m70";
+var DB_PATH = "mongodb://admin:12345678@ds153239.mlab.com:53239/heroku_j86p4m70";
 
 module.exports = {
     conn: function (callback) {
