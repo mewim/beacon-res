16f10b9bd5a4e62e8dcc3fb1cbce4edfb85fbf5f
Liu, Chang <cliu81@illinois.edu>
2017-02-14 13:04:10 -0600

    Test HTTP get.

--- api/beacons.js
+++ api/beacons.js
@@ -1,8 +1,11 @@
 var express = require('express'),
     router = express.Router();
-
+var http = require('http');
 var db = require("../db");
+
 const beacons = db.get('beacons');
+const beacons_version = db.get('beacons_version');
+
 
 router.get('/', function (req, res) {
     res.status(200).send({
@@ -10,5 +13,32 @@ router.get('/', function (req, res) {
         message: "APIs are up and running."
     });
 });
+
+router.get('/sync', function(req, res){
+    beacons_version.drop();
+    beacons.drop();
+    download_version(function(version_data){
+        console.log(version_data);
+    });
+
+});
+
+function download_version(callback){
+    http.request({
+        host: 'minrva-dev.library.illinois.edu',
+        path: '/estimote/rest/v1.0/version',
+        port: '8080',
+        method: 'GET'
+    }, function (res) {
+        var body = '';
+        res.on('data', function(d) {
+            body += d;
+        });
+        res.on('end', function() {
+            var parsed = JSON.parse(body);
+            callback(parsed);
+        });
+    });
+}
 
 module.exports = router;
