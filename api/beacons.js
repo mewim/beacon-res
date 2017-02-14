94685a93c4b4ca7915923d3858f5934d6cc7457f
Liu, Chang <cliu81@illinois.edu>
2017-02-14 12:28:41 -0600

    Add MongoDB.

--- api/beacons.js
+++ api/beacons.js
@@ -2,10 +2,10 @@ var express = require('express'),
     router = express.Router();
 
 router.get('/', function (req, res) {
- res.status(200).send({
-                success: true,
-                message: "APIs are up and running."
-            });
+    res.status(200).send({
+        success: true,
+        message: "APIs are up and running."
+    });
 });
 
 module.exports = router;
--- api/index.js
+++ api/index.js
@@ -2,17 +2,17 @@ var express = require('express'),
     router = express.Router();
 
 router.post('/', function (req, res) {
- res.status(200).send({
-                success: true,
-                message: "APIs are up and running."
-            });
+    res.status(200).send({
+        success: true,
+        message: "APIs are up and running."
+    });
 });
 
 router.get('/', function (req, res) {
- res.status(200).send({
-                success: true,
-                message: "APIs are up and running."
-            });
+    res.status(200).send({
+        success: true,
+        message: "APIs are up and running."
+    });
 });
 
 router.use('/beacons', require('./beacons.js'));
