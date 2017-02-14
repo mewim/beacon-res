a203bdb2bb378bcb15086b62ae2e57aa3dc4da03
Liu, Chang <cliu81@illinois.edu>
2017-02-14 11:42:59 -0600

    Bug fix for hello world API.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('port', (process.env.PORT || 5000));
 // Serve static assets from the /public folder
 app.use(express.static('public'));
 
-app.use('/api', require('/api/index.js'));
+app.use('/api', require('./api/index.js'));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
